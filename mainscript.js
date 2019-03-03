4972d021ade67bec5cf7024bc001d788b6dd335e
wwwbrunagon <wwwbrunagon@gmail.com>
2019-03-03 20:08:28 -0300

    Compare the Triplets with conditional ternary

--- mainscript.js
+++ mainscript.js
@@ -11,8 +11,9 @@ let ar = [1, 2, 3, 4, 10, 11 ]
 //   }
 //   return soma
 // }
-//-------
-//sample 02 function simpleArraySum(ar) {} with forOf
+
+//-------//sample 02 function simpleArraySum(ar) {} with forOf
+
 // function simpleArraySum(ar) {
 //   let soma = 0
 //   for(let a of ar){
@@ -20,12 +21,25 @@ let ar = [1, 2, 3, 4, 10, 11 ]
 //   }return soma
 // } 
 // console.log(simpleArraySum(ar))
-//-------
-//sample 03 function simpleArraySum(ar) {} with 
+
+//-------//sample 03 function simpleArraySum(ar) {} with reduce
+
 function simpleArraySum(ar) {
-  return ar.reduce(function(a,b) {return a+b})
+  return ar.reduce((soma,a) => {return soma+a})
 } 
 console.log(simpleArraySum(ar))
+//we use the reduce to get all the values of an array and make them unique
 
 
-//------------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+
+// Sample the compareTriplets 
+// a = [17, 28, 30] | b = [99, 16, 8]
+// Comparing the 0th  elements, 17<99  so [b] receives a point. 
+// Comparing the  1st and 2st  elements, 28>16 and 30>8 so [a] receives two points. 
+// The return array is [2,1]
+
+function compareTriplets(a, b) {
+
+
+}
